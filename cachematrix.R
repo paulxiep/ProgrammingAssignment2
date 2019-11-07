5d9c6a73d30b54f3a0949aa992b570c2b3c7af8c
paulxiep <56533947+paulxiep@users.noreply.github.com>
2019-11-07 22:04:07 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix transforms the square matrix into an array, adding a 3rd dimension (so x by x matrix becomes x by x by 2)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+array(c(x,matrix(0/0,dim(x),dim(x))),dim=c(dim(x),2))
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the matrix tranformed by makeCachematrix, and stores the inverse in [,,2] dimension
+## If the inverse already exists in the [,,2] dimension, it simply retrieves the stored value, no computation required.
+## output is of the same dimension as the original matrix (before being transformed by makeCacheMatrix)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if(is.nan(x[1,1,2])){
+    x<-array(c(x[,,1],solve(x[,,1])),dim=dim(x))
+  }
+  x[,,2]
 }
